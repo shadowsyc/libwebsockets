0615f48109fff56f00e26ed62b33212e4a7ce684
James Chen <jianhua.chen@cocos2d-x.org>
2013-05-08 18:23:25 +0800

    bug fixes.

--- ios/build_framework.py
+++ ios/build_framework.py
@@ -38,10 +38,9 @@ def build_opencv(srcroot, buildroot, target, arch):
     # for some reason, if you do not specify CMAKE_BUILD_TYPE, it puts libs to "RELEASE" rather than "Release"
     cmakeargs = ("-GXcode " +
                 "-DCMAKE_TOOLCHAIN_FILE=%s/ios/cmake/Toolchains/Toolchain-%s_Xcode.cmake " +
-                "-DWITH_SSL=0" +
+                "-DWITH_SSL=0 " +
                 "-DWITHOUT_SERVER=1 " +
                 "-DWITHOUT_TESTAPPS=1 " +
-                "-DWITHOUT_EXTENSIONS=1 " +
                 "-DCMAKE_INSTALL_PREFIX:PATH=/Users/james/Project/libwebsockets/install") % (srcroot, target)
     # if cmake cache exists, just rerun cmake to update OpenCV.xproj if necessary
     if os.path.isfile(os.path.join(builddir, "CMakeCache.txt")):
