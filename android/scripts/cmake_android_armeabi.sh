482c279b5daedea61288e4e50944f46b01888254
James Chen <jianhua.chen@cocos2d-x.org>
2013-08-01 11:42:14 +0800

    Updating script for android.

--- android/scripts/cmake_android_armeabi.sh
+++ android/scripts/cmake_android_armeabi.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
-cd `dirname $0`/..
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+OUTPUT=$DIR/..
+cd $OUTPUT
+
+mkdir -p $OUTPUT/dist
 
 mkdir -p build_armeabi
 cd build_armeabi
 
-cmake -DANDROID_ABI=armeabi -DCMAKE_TOOLCHAIN_FILE=../android.toolchain.cmake     -DWITH_SSL=0 -DWITHOUT_SERVER=1 -DCMAKE_INSTALL_PREFIX:PATH=/Users/james/Project/libwebsockets/install/android/armv5 $@ ../..
+cmake -DANDROID_ABI=armeabi -DCMAKE_TOOLCHAIN_FILE=../android.toolchain.cmake     -DWITH_SSL=0 -DWITHOUT_SERVER=1 -DCMAKE_INSTALL_PREFIX:PATH=$OUTPUT/dist/armv5 $@ ../..
 make
 make install
 
@@ -13,7 +18,7 @@ cd ../
 mkdir -p build_armeabi-v7a
 cd build_armeabi-v7a
 
-cmake -DANDROID_ABI=armeabi-v7a -DCMAKE_TOOLCHAIN_FILE=../android.toolchain.cmake -DWITH_SSL=0 -DWITHOUT_SERVER=1 -DCMAKE_INSTALL_PREFIX:PATH=/Users/james/Project/libwebsockets/install/android/armv7a $@ ../..
+cmake -DANDROID_ABI=armeabi-v7a -DCMAKE_TOOLCHAIN_FILE=../android.toolchain.cmake -DWITH_SSL=0 -DWITHOUT_SERVER=1 -DCMAKE_INSTALL_PREFIX:PATH=$OUTPUT/dist/armv7a $@ ../..
 make
 make install
 
@@ -21,7 +26,7 @@ cd ../
 mkdir -p build_x86
 cd build_x86
 
-cmake -DANDROID_ABI=x86 -DCMAKE_TOOLCHAIN_FILE=../android.toolchain.cmake -DWITH_SSL=0 -DWITHOUT_SERVER=1 -DCMAKE_INSTALL_PREFIX:PATH=/Users/james/Project/libwebsockets/install/android/x86 $@ ../..
+cmake -DANDROID_ABI=x86 -DCMAKE_TOOLCHAIN_FILE=../android.toolchain.cmake -DWITH_SSL=0 -DWITHOUT_SERVER=1 -DCMAKE_INSTALL_PREFIX:PATH=$OUTPUT/dist/x86 $@ ../..
 make
 make install
 
